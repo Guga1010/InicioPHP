b16a793efc39ea1b3959b288bd5e88a382dc0f09
Guga1010 <joaogustavosdleo@gmail.com>
2022-04-09 12:08:59 -0300

    retirada de uns comentários irrelevante

--- index.php
+++ index.php
@@ -4,16 +4,12 @@ $categorias = [];
 $categorias[] = 'infantil';
 $categorias[] = 'adolescente';
 $categorias[] = 'adulto';
-//print_r($categorias); --> Imprime na tela a variável categorias
 
 $nome = 'Eduardo';
 $idade = 8;
-//var_dump($nome); --> Mostra o tipo da variável nome e a quantidade
-//var_dump($idade); --> Mostra o tipo da variável idade 
 
 if($idade >= 6 && $idade <= 12)
 {
-    // echo 'infantil'; -->Imprime na tela o texto infantil
     for($i = 0; $i < count($categorias); $i++)
     {
         if($categorias[$i] == 'infantil')
